439a9dffdccf494c9934df8afc6b255abe5cd817
Andrey Voytenkov <andrey.voytenkov@flant.com>
2026-02-02 11:50:35 +0200

    Fix some errors in task7

--- Task7/verify/verify-admission.sh
+++ Task7/verify/verify-admission.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+echo "---------------------------------------- Creating namespace -------------------------------------------"
 kubectl apply -f ./../01-create-namespace.yaml
-
+echo
 # указываем неймспейс audit-zone, проверяем PSA, ограничений OPA Gateway пока нет
+echo "---------------------------------------- Testing insecure pods ----------------------------------------"
 kubectl apply -f ./../insecure-manifests -n audit-zone
+echo
+echo "---------------------------------------- Testing secure pod -------------------------------------------"
 kubectl apply -f ./../secure-manifests -n audit-zone
-
+echo
 # для идемпотентности зачищаем всё
+echo "---------------------------------------- Deleting namespace -------------------------------------------"
 kubectl delete ns audit-zone
--- Task7/verify/verify-security.sh
+++ Task7/verify/verify-security.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
+echo "---------------------------------------- Creating OPA Gatekeeper constraints --------------------------"
 kubectl apply -f ./../gatekeeper/constraint-templates
-kubectl apply -f ./../gatekeeper/templates
-
+kubectl apply -f ./../gatekeeper/constraints
+echo
 # не указываем неймспейс, установится в default, там нет PSA, но действуют политики OPA Gateway
+echo "---------------------------------------- Testing insecure pods ----------------------------------------"
 kubectl apply -f ./../insecure-manifests
+echo
+echo "---------------------------------------- Testing secure pod -------------------------------------------"
 kubectl apply -f ./../secure-manifests
-
+echo
 # для идемпотентности зачищаем всё
+echo "---------------------------------------- Deleting created pods -----------------------------------------"
+echo
 kubectl delete -f ./../secure-manifests
-kubectl delete -f ./../gatekeeper/templates
+echo "---------------------------------------- Deleting OPA Gatekeeper constraints ---------------------------"
+kubectl delete -f ./../gatekeeper/constraints
